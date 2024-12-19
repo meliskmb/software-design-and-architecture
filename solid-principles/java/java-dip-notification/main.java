cb13acf498a10f663e80e5f6ca3cea51d6303783
Melis <kmbmelis@gmail.com>
2024-12-19 18:42:32 +0300

    Remove extra tab space from main.java

--- solid-principles/java/java-dip-notification/main.java
+++ solid-principles/java/java-dip-notification/main.java
@@ -2,7 +2,6 @@ public class main{
     public static void main(String[] args){
         User user = new User("user1", "user1@gmail.com", 111, 0000);
         Alert alert = new Alert();
-                alert.notify(user, "test message");
-
+        alert.notify(user, "test message");
     }
 }

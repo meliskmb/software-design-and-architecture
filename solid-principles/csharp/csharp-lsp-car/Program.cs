dc382e389f59b97d68ce70e76e8bbfe3fcf770dd
Melis <kmbmelis@gmail.com>
2024-12-21 01:46:20 +0300

    Initial commit for csharp-lsp-car

---
+++ solid-principles/csharp/csharp-lsp-car/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YTM
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Ferrari ferrari = new Ferrari(0, 0);
+            Console.WriteLine("FERRARI:");
+            ferrari.Power();
+            Console.WriteLine("The vehicle's initial speed: " + ferrari.GetVelocity());
+            ferrari.Run();
+            Console.WriteLine("The vehicle's speed after it started: " + ferrari.GetVelocity());
+            ferrari.Boost();
+            ferrari.Run();
+            Console.WriteLine("The car's speed after the boost: " + ferrari.GetVelocity());
+            ferrari.Open();
+            ferrari.Run();
+            Console.WriteLine("The car's speed after the AC is turned on: " + ferrari.GetVelocity());
+
+            Mercedes mercedes = new Mercedes(0, 0);
+            Console.WriteLine("MERCEDES:");
+            mercedes.Power();
+            Console.WriteLine("The vehicle's initial speed: " + mercedes.GetVelocity());
+            mercedes.Run();
+            Console.WriteLine("The vehicle's speed after it started: " + mercedes.GetVelocity());
+            mercedes.Boost();
+            mercedes.Run();
+            Console.WriteLine("The car's speed after the boost: " + mercedes.GetVelocity());
+
+            Tofas tofas = new Tofas(0, 0);
+            Console.WriteLine("TOFAS:");
+            tofas.Power();
+            Console.WriteLine("The vehicle's initial speed: " + tofas.GetVelocity());
+            tofas.Run();
+            Console.WriteLine("The vehicle's speed after it started: " + tofas.GetVelocity());
+            tofas.Open();
+            tofas.Run();
+            Console.WriteLine("The car's speed after the AC is turned on: " + tofas.GetVelocity());
+        }
+    }
+}
